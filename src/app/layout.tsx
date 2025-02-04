351bd1a449ff6b477c94324585cc40faffd204a2
Areesha Nadeem <AreeshaNadeem973>
2025-02-05 03:14:37 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import {ClerkProvider, SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from "@clerk/nextjs"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider >
     <html lang="en">
       <body className={inter.className}>
+     
         <Header />
         {children}
         <Footer />
       </body>
     </html>
+    </ClerkProvider>
   );
 }
---
+++ src/middleware.ts
@@ -0,0 +1,33 @@
+
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+// Define public routes
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)', 
+  '/', 
+  '/shoplist', 
+//   '/shoplist/(.*)', // This will match dynamic routes inside shoplist, including [id]
+  '/Blog',
+  '/About',
+  '/Chef',
+  '/ContactUs',
+  '/Menu',
+  '/Error',
+  '/Wishlist'
+]);
+
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect(); // Protect all other routes
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
